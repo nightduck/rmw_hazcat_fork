54dc701ba3c236fa1b33de2c8cfd3e4988ab3e59
oren <virtualoren@gmail.com>
2022-03-07 09:22:11 -0600

    Pushing so I can see this at the office

--- include/rmw_hazcat_cpp/allocators/cpu_pool_allocator.hpp
+++ include/rmw_hazcat_cpp/allocators/cpu_pool_allocator.hpp
@@ -1,14 +1,15 @@
 #include "rmw_hazcat_cpp/allocators/hma_template.hpp"
 
-template<class T> 
+template<class T, size_t POOL_SIZE> 
 class StaticPoolAllocator : public HMAAllocator<CPU_Mem> {
 public:
-    StaticPoolAllocator(size_t pool_size) {
-        forward_it = rear_it = 0;
-        size = pool_size;
-        pool = this + 3 * sizeof(int);  // Pool takes up remainder of class size
+    StaticPoolAllocator() {
+        forward_it = 0;
+        rear_it = -1;
+        dealloc_fn = &this->static_deallocate;
     }
 
+    // TODO: Reconsider starting conditions of iterators being -1 and 0
     // Allocates a chunk of memory. Argument is a syntactic formality, will be ignored
     void * allocate(size_t size = sizeof(T)) {
         int next = (forward_it + 1 >= size) ? 0 : forward_it + 1;
@@ -16,15 +17,39 @@ public:
             return NULL;
         } else {
             forward_it = next;
-            return &pool[next];
+            return &pool[next] - this;  // Give address relative to shared object
         }
     }
 
+    static void static_deallocate(StaticPoolAllocator * alloc, void * ptr) {
+        alloc->deallocate(ptr);
+    }
+
 private:
-    
+    // TODO: Reconsider starting conditions of iterators being -1 and 0
+    void deallocate(void * ptr) {
+        int index = (ptr - &pool) / sizeof(T);
+        if(forward_it > index && index > rear_it) {
+            rear_it = index;
+            return;
+        } else if (index > rear_it && rear_it > forward_it) {
+            rear_it = index;
+            return;
+        } else if (rear_it > forward_it && forward_it > index) {
+            rear_it = index;
+            return;
+        }
+    }
+
+
+    void * copy_from(void * ptr, int size) {
+        return ptr;
+    }
+    void * copy_from(void * ptr, int size) {
+        return ptr;
+    }
 
     int forward_it;
     int rear_it;
-    int size;
-    T pool[];
+    T pool[POOL_SIZE];
 };
--- include/rmw_hazcat_cpp/allocators/hma_template.hpp
+++ include/rmw_hazcat_cpp/allocators/hma_template.hpp
@@ -1,17 +1,17 @@
 #include <sys/shm.h>
 
 typedef CPU_Mem;
-typedef GPU_Mem;
+typedef CUDA_Mem;
 
 template<typename MemoryDomain>
 class HMAAllocator {
 public:
     // Takes same arguments as constructor, allocates shared memory that may be used by constructor
     // with placement new
-    template<typename... Args>
-    static void * create_alloc(Args... args) {
+    template<typename AllocT, typename... Args>
+    static AllocT * create_shared_alloc(Args... args) {
         int key = 56;
-        size_t size = compute_size(args);
+        size_t size = sizeof(AllocT);
 
         if(!shmget(key, size, IPC_CREAT)) {
             // TODO: More robust error checking
@@ -21,10 +21,18 @@ public:
         // TODO: shmctl to set permissions and such
 
         // TODO: Link the library for the allocator so code is visible too
-        return shmat(key, NULL, 0);
+        void * ptr = shmat(key, NULL, 0);
+        return new(ptr) AllocT(args);
     }
+
+    // Reserve address space for self and optional memory pool
+    // Map both shared memory object containg self and memory pool into reservation
+    static void * map_shared_alloc(int shm_id);
     
-    void * allocate(size_t size);
+    virtual void * allocate(size_t size);
+
+    // Static wrapper for deallocate
+    static virtual void static_deallocate(void * alloc, void * ptr); // {alloc->deallocate(ptr);}
 
     template<typename T>
     void * convert(void* ptr, int size, HMAAllocator<T> * alloc) {
@@ -39,17 +47,21 @@ public:
         }
     }
 
+    void * resolve(long token);
+
 private:
     int shmem_id;
+    void (*dealloc_fn)(void*,void*);    // Set to static_deallocate
+
+    virtual void deallocate(void * ptr);
+
     virtual void * copy_from(void * ptr, int size);
     virtual void * copy_to(void * ptr, int size);
-
-    // Takes same arguments as constructor and computes it's final size
-    template<typename... Args>
-    virtual int compute_size(Args... args);
     
+    // Default copy operation between to non-CPU domains, using CPU as intermediary.
+    // Can override with type-specific implementations
     template<typename T>
     void * copy(void * ptr, int size, HMAAllocator<T> * alloc) {
-
+        return alloc->copy_to(this->copy_from(ptr, size), size);
     }
 };
